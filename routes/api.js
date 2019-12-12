c8eb6825a2a7816a5d7d0d4cb521c3863040ae6b
VKen <vken85@gmail.com>
2019-12-12 20:56:28 +0800

    setup basic mongodb connection

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@
 *
 *
 *       Complete the API routing below
-*       
-*       
+*
+*
 */
 
 'use strict';
@@ -12,42 +12,52 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
-//Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
+const mongo = new MongoClient(MONGODB_CONNECTION_STRING, { useUnifiedTopology: true });
+
+(async function() {
+    await mongo.connect((err, client) => {
+        if (err) {
+            console.error('connection error to db');
+        } else {
+            console.log('db connection successful');
+        }
+    });
+})()
 
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get(async function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(async function (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(async function(req, res){
       //if successful response will be 'complete delete successful'
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res){
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(async function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(async function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
+
 };
